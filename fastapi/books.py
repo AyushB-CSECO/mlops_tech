d653a1fcc071151e10163df7e753c856cb49e350
Ayush Bansal <58684774+AyushB-CSECO@users.noreply.github.com>
2026-08-02 10:25:41 +0000

    Created FastAPI with dynamic parameter

--- fastapi/books.py
+++ fastapi/books.py
@@ -2,6 +2,20 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def greet_user():
-    return {"message": "Hello, World"}
+BOOKS = [
+    {'title':"T_1", 'author':"Auth_1", 'category': "Fiction"},
+    {'title':"T_2", 'author':"Auth_2", 'category': "Self Help"},
+    {'title':"T_3", 'author':"Auth_3", 'category': "Fiction"},
+    {'title':"T_4", 'author':"Auth_4", 'category': "Mythology"},
+    {'title':"T_5", 'author':"Auth_5", 'category': "Fiction"},
+    {'title':"T_6", 'author':"Auth_6", 'category': "Self Help"}
+]
+
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
+
+# Create API with dynamic parameter
+@app.get("/books/{dynamic_param}")
+async def read_book(dynamic_param):
+    return {"dynamic_param": dynamic_param}
