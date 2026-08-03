92afa477c910899758cb233e75f7b35e66943502
Ayush Bansal <58684774+AyushB-CSECO@users.noreply.github.com>
2026-08-03 06:23:40 +0000

    Created simple GET & POST APIs using FastAPI

--- fastapi/books.py
+++ fastapi/books.py
@@ -1,21 +1,50 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
 BOOKS = [
-    {'title':"T_1", 'author':"Auth_1", 'category': "Fiction"},
-    {'title':"T_2", 'author':"Auth_2", 'category': "Self Help"},
-    {'title':"T_3", 'author':"Auth_3", 'category': "Fiction"},
-    {'title':"T_4", 'author':"Auth_4", 'category': "Mythology"},
-    {'title':"T_5", 'author':"Auth_5", 'category': "Fiction"},
-    {'title':"T_6", 'author':"Auth_6", 'category': "Self Help"}
+    {'title':"T 1", 'author':"Auth_1", 'category': "Fiction"},
+    {'title':"T 2", 'author':"Auth_2", 'category': "Self Help"},
+    {'title':"T 3", 'author':"Auth_3", 'category': "Fiction"},
+    {'title':"T 4", 'author':"Auth_4", 'category': "Mythology"},
+    {'title':"T 5", 'author':"Auth_5", 'category': "Fiction"},
+    {'title':"T 6", 'author':"Auth_6", 'category': "Self Help"}
 ]
 
 @app.get("/books")
 async def read_all_books():
     return BOOKS
 
-# Create API with dynamic parameter
-@app.get("/books/{dynamic_param}")
-async def read_book(dynamic_param):
-    return {"dynamic_param": dynamic_param}
+# Get a particular book dynamically
+@app.get("/books/{book_title}")
+async def read_book(book_title):
+    for book in BOOKS:
+        if book['title'] == book_title.upper():
+            return book
+
+# Return all the books of a specific category
+@app.get("/books/")
+async def read_category_by_query(category: str) -> list:
+    output = []
+    for book in BOOKS:
+        if book['category'] == category.title():
+            output.append(book)
+    return output
+
+# Return all books with queried author and category
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(
+    book_author: str,
+    category: str
+    ) -> list:
+    output = []
+    for book in BOOKS:
+        if ( (book['author'] == book_author.title())  
+            and (book['category'] == category.title())): 
+            output.append(book)
+    return output
+
+# Create a POST API to add new book to the books list
+@app.post("/books/create_book")
+async def create_book(new_book = Body()):
+    BOOKS.append(new_book)
