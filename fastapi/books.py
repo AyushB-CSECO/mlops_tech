89a771b5096b5ac2e9119ccfa5b628a642eab045
Ayush Bansal <58684774+AyushB-CSECO@users.noreply.github.com>
2026-08-06 05:02:10 +0000

    Added pydantic data validation to BookRequest class

--- fastapi/books.py
+++ fastapi/books.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, Body
-from pydantic import BaseModel
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from typing import Optional
 
 app = FastAPI()
 
@@ -19,11 +20,11 @@ class Book:
 
 # Create a class for new books from user. Use pydantic for validation
 class BookRequest(BaseModel):
-    id: int 
-    title: str
-    author: str
-    description: str
-    rating: int
+    id: Optional[int] = Field(description = "ID is not necessary at create", default=None)
+    title: str = Field(min_length=3)
+    author: str = Field(min_length= 1)
+    description: str = Field(min_length=1, max_length=100 )
+    rating: int = Field(gt=0, lt=6)
 
 BOOKS = [
     Book(1, "Animal Farm", "George Orwell", "Political Satire", 5),
@@ -34,6 +35,10 @@ BOOKS = [
     Book(6, "Let Us C", "Yashwant Kanetkar", "Book on C++ coding", 4)
 ]
 
+def find_book_id(book: Book):
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+    return book 
+
 @app.get("/books")
 async def read_all_books():
     return BOOKS
@@ -42,6 +47,7 @@ async def read_all_books():
 @app.post("/create-book")
 async def create_book(book_request: BookRequest):
     new_book = Book(**book_request.model_dump())
+    new_book = find_book_id(new_book)
     BOOKS.append(new_book)
 
 
