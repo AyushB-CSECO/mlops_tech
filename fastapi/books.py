31a1bfa11b42459d75bbc77d0a8da0ef278c9858
Ayush Bansal <58684774+AyushB-CSECO@users.noreply.github.com>
2026-08-06 07:53:24 +0000

    Added publish_date to Book class. Created GET filter, PUT and DELETE APIs

--- fastapi/books.py
+++ fastapi/books.py
@@ -10,13 +10,16 @@ class Book:
     author: str
     description: str
     rating: int
+    published_date: int
 
-    def __init__(self, id, title, author, description, rating):
+    def __init__(self, id, title, author, description
+                 , rating, published_date):
         self.id = id 
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.published_date = published_date
 
 # Create a class for new books from user. Use pydantic for validation
 class BookRequest(BaseModel):
@@ -25,14 +28,15 @@ class BookRequest(BaseModel):
     author: str = Field(min_length= 1)
     description: str = Field(min_length=1, max_length=100 )
     rating: int = Field(gt=0, lt=6)
+    published_date: int = Field(lt=2027)
 
 BOOKS = [
-    Book(1, "Animal Farm", "George Orwell", "Political Satire", 5),
-    Book(2, "Atomic Habbits", "James Clear", "Helps in building good habbits", 4),
-    Book(3, "Five Point Someone", "Chetan Bhagat", "Story about College Friends", 3),
-    Book(4, "Revolution 2020", "Chetan Bhagat", "Struggles of an average middle class boy", 4),
-    Book(5, "The Rudest Book Ever", "Shwetabh Gangwar", "Self Help Book on Life", 5),
-    Book(6, "Let Us C", "Yashwant Kanetkar", "Book on C++ coding", 4)
+    Book(1, "Animal Farm", "George Orwell", "Political Satire", 5, 1947),
+    Book(2, "Atomic Habbits", "James Clear", "Helps in building good habbits", 4, 2004),
+    Book(3, "Five Point Someone", "Chetan Bhagat", "Story about College Friends", 3, 2009),
+    Book(4, "Revolution 2020", "Chetan Bhagat", "Struggles of an average middle class boy", 4, 2010),
+    Book(5, "The Rudest Book Ever", "Shwetabh Gangwar", "Self Help Book on Life", 5, 2016),
+    Book(6, "Let Us C", "Yashwant Kanetkar", "Book on C++ coding", 4, 1999)
 ]
 
 def find_book_id(book: Book):
@@ -43,6 +47,23 @@ def find_book_id(book: Book):
 async def read_all_books():
     return BOOKS
 
+
+@app.get("/books/{book_id}")
+async def read_book(book_id:int):
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+
+@app.get("/books/")
+async def read_book_by_rating(rating: int):
+    output = [book for book in BOOKS if book.rating == rating]
+    return output
+
+@app.get("/books/publish/{published_date}")
+async def read_book_by_date(published_date: int):
+    output = [book for book in BOOKS if book.published_date == published_date]
+    return output
+
 # Create POST API to create book
 @app.post("/create-book")
 async def create_book(book_request: BookRequest):
@@ -50,81 +71,15 @@ async def create_book(book_request: BookRequest):
     new_book = find_book_id(new_book)
     BOOKS.append(new_book)
 
+@app.put("/books/update_book")
+async def update_book(book: BookRequest):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
 
-
-if False:
-    from fastapi import FastAPI, Body
-
-    app = FastAPI()
-
-    BOOKS = [
-        {'title':"T 1", 'author':"Auth_1", 'category': "Fiction"},
-        {'title':"T 2", 'author':"Auth_2", 'category': "Self Help"},
-        {'title':"T 3", 'author':"Auth_1", 'category': "Fiction"},
-        {'title':"T 4", 'author':"Auth_3", 'category': "Mythology"},
-        {'title':"T 5", 'author':"Auth_1", 'category': "Fiction"},
-        {'title':"T 6", 'author':"Auth_2", 'category': "Self Help"}
-    ]
-
-    @app.get("/books")
-    async def read_all_books():
-        return BOOKS
-
-    # Get a particular book dynamically
-    @app.get("/books/{book_title}")
-    async def read_book(book_title):
-        for book in BOOKS:
-            if book['title'] == book_title.upper():
-                return book
-
-    # Return all the books of a specific category
-    @app.get("/books/")
-    async def read_category_by_query(category: str) -> list:
-        output = []
-        for book in BOOKS:
-            if book['category'] == category.title():
-                output.append(book)
-        return output
-
-    # Return all books with queried author and category
-    @app.get("/books/{book_author}/")
-    async def read_author_category_by_query(
-        book_author: str,
-        category: str
-        ) -> list:
-        output = []
-        for book in BOOKS:
-            if ( (book['author'] == book_author.title())  
-                and (book['category'] == category.title())): 
-                output.append(book)
-        return output
-
-    # Create a POST API to add new book to the books list
-    @app.post("/books/create_book")
-    async def create_book(new_book = Body()):
-        BOOKS.append(new_book)
-
-    # Create a PUT API to update existing book
-    @app.put("/books/update_book")
-    async def update_book(updated_book = Body()):
-        for i in range(len(BOOKS)):
-            if BOOKS[i].get("title") == updated_book.get('title').upper():
-                BOOKS[i] = updated_book
-
-    # Create a DELETE API to delete an existing book
-    @app.delete("/books/delete_book/{book_title}")
-    async def delete_book(book_title:str):
-        for i in range(len(BOOKS)):
-            if BOOKS[i].get("title") == book_title.upper():
-                BOOKS.pop(i)
-                break
-
-    # Create a GET API that can fetch all books from a specific author
-    # using either Path Parameter or Query Parameter
-    @app.get("/books/get_author/{author}")
-    async def get_books_by_author_path(author:str):
-        output = []
-        for book in BOOKS:
-            if book.get("author") == author.title():
-                output.append(book)
-        return output
+@app.delete("/books/delete_book")
+async def delete_book(book_id:int):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_id:
+            BOOKS.pop(i)
+            break
