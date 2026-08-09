89cb70236c8f0494a2b04dfb18bdc6e36724ec1b
Ayush Bansal <58684774+AyushB-CSECO@users.noreply.github.com>
2026-08-09 18:09:21 +0000

    Changed SQLAlchemy DB path. Now looks at database.py file than working directory to find relative path for DB

--- fastapi/TodoApp/database.py
+++ fastapi/TodoApp/database.py
@@ -1,9 +1,11 @@
+from pathlib import Path
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # Database URL
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+BASE_DIR = Path(__file__).resolve().parent
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{BASE_DIR}/todos.db"
 connect_args_dict = {"check_same_thread":False}
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=connect_args_dict)
