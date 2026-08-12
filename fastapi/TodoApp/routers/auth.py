c4c0e799c7449d8f893b2f04428eb6baa7249f00
Ayush Bansal <58684774+AyushB-CSECO@users.noreply.github.com>
2026-08-12 18:27:44 +0000

    Hashed user password. Save users in DB

--- fastapi/TodoApp/routers/auth.py
+++ fastapi/TodoApp/routers/auth.py
@@ -1,8 +1,16 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 from pydantic import BaseModel 
+from argon2 import PasswordHasher
+from starlette import status
+
 from ..models import Users
+from ..database import session_local
 
 router = APIRouter()
+
+password_hasher = PasswordHasher()
 
 class CreateUserRequest(BaseModel):
     username: str
@@ -12,16 +20,27 @@ class CreateUserRequest(BaseModel):
     password: str
     role: str
 
-@router.post("/auth")
-async def create_user(create_user_request: CreateUserRequest):
+def get_db():
+    db = session_local()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@router.post("/auth", status_code = status.HTTP_201_CREATED)
+async def create_user(db:db_dependency, 
+                        create_user_request: CreateUserRequest):
     create_user_model = Users(
         email = create_user_request.email,
         username = create_user_request.username,
         first_name = create_user_request.first_name,
         last_name = create_user_request.last_name,
-        hashed_password = create_user_request.password,
+        hashed_password = password_hasher.hash(create_user_request.password),
         role = create_user_request.role,
         is_active = True
     )
 
-    return create_user_model
+    db.add(create_user_model)
+    db.commit()
