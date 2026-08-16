3a0ea6e0d427894af9c82cda63aeaaafb803fb3b
Ayush Bansal <58684774+AyushB-CSECO@users.noreply.github.com>
2026-08-16 15:50:02 +0000

    Added CSS styling but not working

--- fastapi/Blog/main.py
+++ fastapi/Blog/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
+app.mount('/static', StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
@@ -26,7 +29,9 @@ posts: list[dict] = [
 @app.get("/", include_in_schema=False)
 @app.get("/posts", include_in_schema=False)
 def home(request: Request):
-    return templates.TemplateResponse(request,"home.html", {"posts":posts})
+    return templates.TemplateResponse(request,"home.html"
+                , {"posts":posts, 'title':'Home'}
+    )
 
 @app.get("/api/posts")
 def get_posts():
