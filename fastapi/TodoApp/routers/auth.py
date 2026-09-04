831f1fb00ac5f059064cec11dfd2a4259d6050a0
Ayush Bansal <58684774+AyushB-CSECO@users.noreply.github.com>
2026-09-04 17:28:38 +0000

    Added code to authenticate user

--- fastapi/TodoApp/routers/auth.py
+++ fastapi/TodoApp/routers/auth.py
@@ -4,9 +4,19 @@ from argon2 import PasswordHasher
 from typing import Annotated
 from sqlalchemy.orm import Session
 from starlette import status
+from fastapi.security import OAuth2PasswordRequestForm
 
 from models import Users
 from database import SessionLocal
+
+
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
 
 
 # Create Database object
@@ -17,27 +27,53 @@ def get_db():
     finally:
         db.close()
 
-class CreateUserRequest(BaseModel):
-    username: str
-    email: str
-    first_name: str
-    last_name: str
-    password: str
-    role: str
-
 # Password Hasher
-pwd_hasher = PasswordHasher()
+pwd_context = PasswordHasher()
 
+# Converts password into hashed password
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+# Compare and verify user provided plain password
+# against hashed password 
+def verify_password(plain_password: str, hashed_password: str):
+    try:
+        pwd_context.verify(hashed_password, plain_password)
+        return True 
+    except Exception:
+        return False
+
+# Authenticate a user against his/her provided credentials
+def authenticate_user(username:str, password:str, db):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return True
+
+
+# Define APIRouter from FastAPI. This will be 
+# included in main app. 
 router = APIRouter()
 
 @router.post("/auth", status_code=status.HTTP_201_CREATED)
 async def create_user(create_user_request: CreateUserRequest,
                         db: Annotated[Session, Depends(get_db)]):
     user_request_dict = create_user_request.model_dump()
-    hashed_password = pwd_hasher.hash(user_request_dict['password'])
+    hashed_password = hash_password(user_request_dict['password'])
     user_request_dict['hashed_password'] = hashed_password
     user_request_dict.pop('password', None)
 
     create_user_model = Users(**user_request_dict)
     db.add(create_user_model)
     db.commit()
+
+@router.post("/token")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                                 db: Annotated[Session, Depends(get_db)]):
+    is_verified = authenticate_user(form_data.username, form_data.password, db)
+
+    if not is_verified:
+        return "Failed Authentication"
+    return "Successful Authentication"
